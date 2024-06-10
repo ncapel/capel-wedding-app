cf2984d7e5b5daa3c631f90ca520fc0520a15197
ncapel <capel.nicholas@gmail.com>
2024-06-10 13:15:25 -0400

    Adding background image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './Components/Nav';
 import Header from './Components/Header';
+import BackgroundImage from './Components/Background';
 
 function App() {
   return (
---
+++ src/Components/Background.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function BackgroundImage() {
+    const bgImage = './assets/img/bg)img.png';
+    return (
+        <img src={bgImage} alt='Gold Frame'></img>
+    );
+}
+
+export default BackgroundImage;
